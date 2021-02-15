0580ae4b63e226feff1788d2a2d170f1f37b0e51
Ofer Zadikario <ofer.zadikario@gmail.com>
2021-02-15 13:40:23 +0200

    CreatedAdd project files.

---
+++ PageNavigationMonkeys/ShowMonkeyPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PageNavigationMonkeys
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ShowMonkeyPage : ContentPage
+    {
+        public ShowMonkeyPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
